29b596ce036535232b7a2d0e79b4e9e4a110a4ce
magfurulabeer <magfurulabeer@gmail.com>
2016-12-21 00:10:30 -0500

    Complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 15, "M", "Brown", "Sweet Tooth", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 10, "M", "Black", "Rowdy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 6, "F", "Blue?", "Smart", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 25, "M", "Brown", "Old", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 8, "F", "Orange", "Fancy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 17, "M", "Green", "Grumpy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 7, "F", "Blonde", "Emo", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (Null, Null, Null, "Rainbow", "Killer", 1);
